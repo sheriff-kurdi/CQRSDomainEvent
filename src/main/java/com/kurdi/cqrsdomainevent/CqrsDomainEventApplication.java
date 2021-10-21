12d59d2529840c0db6f4b4cc0869e5ef5c00f216
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-21 13:44:27 +0200

    Initial commit

---
+++ src/main/java/com/kurdi/cqrsdomainevent/CqrsDomainEventApplication.java
@@ -0,0 +1,13 @@
+package com.kurdi.cqrsdomainevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDomainEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsDomainEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurdi/cqrsdomainevent/CqrsDomainEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurdi.cqrsdomainevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsDomainEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
